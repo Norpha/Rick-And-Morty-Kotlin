c4ceff592dca9a2cc9e3deef844ff86abad4020c
NORM <mac@Norman.local>
2024-10-23 00:32:10 +0300

    duplicate hilt dependencies cleared and versions updated

--- network/src/main/java/com/example/network/repository/TvCharacterRepository.kt
+++ network/src/main/java/com/example/network/repository/TvCharacterRepository.kt
@@ -2,7 +2,11 @@ package com.example.network.repository
 
 import com.example.network.model.TvCharacter
 import com.example.network.model.retrofit.RickAndMortyApi
+import dagger.Provides
 import javax.inject.Inject
+import javax.inject.Singleton
+
+
 
 class TvCharacterRepository @Inject constructor(private val api: RickAndMortyApi) {
 
